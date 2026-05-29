7f9821bb08db75d991d0178c50b88e617820fcca
Joao <joaofssalves@gmail.com>
2026-05-29 11:44:45 +0100

    0.1.2

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+//Notas: criar funcao para 
+
 typedef struct {
     int inicio1, fim1;   /* 1º intervalo */
     int inicio2, fim2;   /* 2º intervalo (opcional) */
@@ -13,20 +15,38 @@ typedef struct nodePessoa {
     char nome[30];
     int numAulas;
     Disp disponiblidade[7];
+    int data;                       //numero de dias desde 1/1/2000
     struct nodePessoa *next;
 } nodePessoa;
 
-void adicionaPessoa(nodePessoa *head) {
-    while (head != NULL) {
+int dataCtoT(int data) {
+
+}
+
+void adicionaPessoa(nodePessoa *head, char nome[], Disp dispo[], int data) {
+    int i;
+    while (head->next != NULL) {
         head = head->next;
     }
-    head->numAulas = 0;
-    head->next=NULL;
-    strcpy(head->nome,"nome");
+    nodePessoa *novo = (nodePessoa *) malloc(sizeof( nodePessoa));
+
+    head->next = novo; 
+    novo->numAulas = 0;
+    novo->next=NULL;
+    strcpy(novo->nome,nome);
+    for (i=0; i==6; i++) {
+        novo->disponiblidade[i].inicio1 = dispo[i].inicio1;
+        novo->disponiblidade[i].fim1 = dispo[i].fim1;
+        novo->data=data;
+        if (novo->disponiblidade[i].numIntervalos==2) {
+            novo->disponiblidade[i].inicio2 = dispo[i].inicio2;
+            novo->disponiblidade[i].fim2 = dispo[i].fim2;
+        }
+    } 
     return;
-    
 };
-int pessoaExiste (nodePessoa P) {};
+
+int pessoaExiste (nodePessoa *head) {};
 void adicionaPessoa (char nome[], int ) {};
 
 
